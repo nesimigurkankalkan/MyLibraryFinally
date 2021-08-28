eb3a43cacddaaacf0ce6159455a2b5c90c57eedf
gurkankalkandesktop <nesimigurkankalkan@gmail.com>
2021-08-29 01:15:27 +0300

    Is Firewall Opened ?

---
+++ MyLibraryFinally/ControlProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace MyLibraryFinally
+{
+    public class ControlProcess
+    {
+        //Using
+        //IsthereFirewall
+        //bool control = ControlProcess.FirewallOpened();
+        public static bool FirewallOpened()
+        {
+            Type t = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+            dynamic dy = Activator.CreateInstance(t);
+            return Convert.ToBoolean(dy.LocalPolicy.CurrentProfile.FirewallEnabled);
+        }
+    }
+}
