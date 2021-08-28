5a7903a6a408fff804a608752824204613f33060
gurkankalkandesktop <nesimigurkankalkan@gmail.com>
2021-08-29 01:17:12 +0300

    Antivirus Installed ?

--- MyLibraryFinally/ControlProcess.cs
+++ MyLibraryFinally/ControlProcess.cs
@@ -15,5 +15,23 @@ namespace MyLibraryFinally
             dynamic dy = Activator.CreateInstance(t);
             return Convert.ToBoolean(dy.LocalPolicy.CurrentProfile.FirewallEnabled);
         }
+        //Using
+        //IsthereAntivirus
+        //bool control = AntivirusInstalled();
+        public static bool AntivirusInstalled()
+        {
+            string wmipathstr = @"\\" + Environment.MachineName + @"\root\SecurityCenter";
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(wmipathstr, "SELECT * FROM AntivirusProduct");
+                ManagementObjectCollection instances = searcher.Get();
+                return instances.Count > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return false;
+        }
     }
 }
