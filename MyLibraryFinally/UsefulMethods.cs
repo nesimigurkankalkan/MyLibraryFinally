c65c2dcbeee8ccf59ab39bf469fa71232a7703a9
gurkankalkandesktop <nesimigurkankalkan@gmail.com>
2021-08-29 02:35:12 +0300

    UsefulMethods Added Class and SeperateWord Method Added

---
+++ MyLibraryFinally/UsefulMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraryFinally
+{
+    public class UsefulMethods
+    {
+        //Using
+        //SeperateWord("Gürkan Nesimi Kalkan");
+        public static string SeperateWord(string word)
+        {
+            string[] s;
+            s = word.Split(' ');
+            string tamad = s[1] + " " + s[0] + " " + s[2];
+            return tamad;
+        }
+
+    }
+}
