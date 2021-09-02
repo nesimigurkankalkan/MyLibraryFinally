9e17b90d4b4190f6d66ed742c8501cd8af0d7c30
gurkankalkancompany <nesimigurkankalkan@gmail.com>
2021-09-02 13:49:19 +0300

    Added Flush Memory Method

--- MyLibraryFinally/OtherMethods.cs
+++ MyLibraryFinally/OtherMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,16 @@ namespace MyLibraryFinally
             string tamad = s[1] + " " + s[0] + " " + s[2];
             return tamad;
         }
-
+        //Using
+        //FlushMemory
+        [DllImportAttribute("kernel32.dll", EntryPoint = "SetProcessWorkingSetSize", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int SetProcessWorkingSetSize(IntPtr process, int minimumWorkingSetSize, int maximumWorkingSetSize);
+        public static void FlushMemory()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                SetProcessWorkingSetSize(System.Diagnostics.Process.GetCurrentProcess().Handle, -1, -1);
+        }
     }
 }
