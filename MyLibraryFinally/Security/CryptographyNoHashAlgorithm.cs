d1f6b0541dd44a3d73c8839251aa24a4d3c97a01
gurkankalkancompany <nesimigurkankalkan@gmail.com>
2021-09-02 13:22:15 +0300

    Added Encrypt with No Hash Algorithm

---
+++ MyLibraryFinally/Security/CryptographyNoHashAlgorithm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraryFinally.Security
+{
+    public class CryptographyNoHashAlgorithm
+    {
+        public static string DoEncrypt(String Text, string key)
+        {
+            String Value = "";
+            Value = Encrypt(Text, key);
+            return Value;
+        }
+
+        public static string DoDecrypt(String Text, string key)
+        {
+            String Value = "";
+            Value = Decrypt(Text, key);
+            return Value;
+        }
+
+        static string Encrypt(string clearText, string Password)
+        {
+            byte[] clearBytes = System.Text.Encoding.Unicode.GetBytes(clearText);
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            byte[] encryptedData = Encrypt(clearBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+            return Convert.ToBase64String(encryptedData);
+        }
+        static string Decrypt(string cipherText, string Password)
+        {
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            byte[] decryptedData = Decrypt(cipherBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+            return System.Text.Encoding.Unicode.GetString(decryptedData);
+        }
+
+
+        static byte[] Encrypt(byte[] clearData, byte[] Key, byte[] IV)
+        {
+            MemoryStream ms = new MemoryStream();
+            Rijndael alg = Rijndael.Create();
+            alg.Key = Key;
+            alg.IV = IV;
+            CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(clearData, 0, clearData.Length);
+            cs.Close();
+            byte[] encryptedData = ms.ToArray();
+            return encryptedData;
+        }
+
+        static byte[] Decrypt(byte[] cipherData, byte[] Key, byte[] IV)
+        {
+            MemoryStream ms = new MemoryStream();
+            Rijndael alg = Rijndael.Create();
+            alg.Key = Key;
+            alg.IV = IV;
+            CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(cipherData, 0, cipherData.Length);
+            cs.Close();
+            byte[] decryptedData = ms.ToArray();
+            return decryptedData;
+        }
+    }
+}
